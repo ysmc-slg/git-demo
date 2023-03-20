5293828401a42e41e94dee6d23b45a57dcb4ff2a
ysmc <494786209@qq.com>
2023-03-20 15:34:50 +0800

    第二次提交

---
+++ src/main/java/top/zxqs/TestGit.java
@@ -0,0 +1,11 @@
+package top.zxqs;
+
+/**
+ * @Author: zxq
+ * @date: 2023-03-20 15:20
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("TestGit");
+    }
+}
